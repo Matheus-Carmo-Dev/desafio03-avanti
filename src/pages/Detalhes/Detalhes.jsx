41547235b2024b81e080359655cdfc02ed72a36d
matheuscarmmo <matheuscarmo.jus@hotmail.com>
2023-09-27 23:28:07 -0300

    mudanças no estilo da página em geral

--- src/pages/Detalhes/Detalhes.jsx
+++ src/pages/Detalhes/Detalhes.jsx
@@ -33,32 +33,32 @@ export const Detalhes = () => {
         <>
             <div className="detalhes-container .d-flex flex-column align-items-center">
             <Header />
-                <h1>{movie.title}</h1>
+                <h2>{movie.title}</h2>
                 <h5>{moment(movie.release_date).format('DD/MM/YYYY')}</h5>
-                <img className="detalhes-icon" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="" />
+                <img className="detalhes-poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="" />
                 <link rel="icon" href="./assets/icone_logo_aba.png" />
                 <h5 className="text-warning " >{movie.vote_average}
                     <FaStar />
 
                 </h5>
-                <div>
-                    <h4>Genero(s)</h4>
+                <div className="genero">
+                    <h5>Gênero(s)</h5>
                     {
                         movie.genres?.map((genre) => (
-                            <span key={genre.id}>{`${genre.name}`} <br /> </span>
+                            <span key={genre.id}>{`${genre.name}`} </span>
                         ))
                     }
                 </div>
 
-                <p className="mt-4">{movie.overview}</p>
+                <p className="description">{movie.overview}</p>
 
-                <div>
-                    <h4>Companhias de produção</h4>
+                <div className="companies">
+                    <h5>Companhias de produção</h5>
                     {
                         movie.production_companies?.map((companies) => (
                             <span key={companies.id}>
                                 {companies.logo_path != undefined &&
-                                    <img className="detalhes-companies" src={`https://image.tmdb.org/t/p/original/${companies.logo_path}`}></img>
+                                    <img className="companies-img" src={`https://image.tmdb.org/t/p/original/${companies.logo_path}`}></img>
                                 }
 
                             </span>
